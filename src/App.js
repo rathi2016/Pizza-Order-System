a20a4794529b618fc5d9eb0f9b8e436aaba9f2b3
Rathi <github email address>
2017-07-25 13:37:52 -0400

    include webpack

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
+var React  = require('react')
+var ReactDOM = require('react-dom')
+
+var App  = React.createClass({
+  render: function(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+      <div>
+        <div id = "header"></div>
 
-export default App;
+        <div className="container">
+          <div className="column">
+              <InboxPane/>
+              <div>
+                  <Welcome />
+              </div>
+
+          </div>
+          <div className="column">
+
+          </div>
+          <div className="column">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var Welcome = React.createClass({
+render: function(){
+  return <h1>Hello,{name}</h1>;
+}
+});
+
+var InboxPane = React.createClass({
+  render: function(){
+    return (
+    <div id ="inbox-pane">
+       <h1>Inbox</h1>
+       <table>
+         <thead>
+           <tr>
+             <th>Chat Received</th>
+             <th>Name</th>
+             <th>Status</th>
+           </tr>
+         </thead>
+            <tbody>
+              <InboxItem/>
+            </tbody>
+       </table>
+    </div>
+  )
+  }
+});
+
+var InboxItem =React.createClass({
+   render: function(){
+     return (
+       <tr>
+         <td>5pm</td>
+         <td>RAthi </td>
+         <td>confirmed</td>
+       </tr>
+     )
+   }
+});
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App
