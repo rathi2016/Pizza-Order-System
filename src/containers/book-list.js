bdb21ed0714d092d7b907cb72f920f5e66d76ce2
Rathi <github email address>
2017-09-12 21:42:02 -0400

    added action container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList(){
@@ -23,4 +25,10 @@ function mapStateToProps(state) {
     books: state.books
   };
 }
-export default connect(mapStateToProps)(BookList);
+
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed
+  // to all of our reducer
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
