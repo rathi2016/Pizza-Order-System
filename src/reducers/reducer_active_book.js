94e8b5842890ffad7de2586a4d1deeecb7c53c82
Rathi <github email address>
2017-09-20 17:02:56 -0400

    added required reducers

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,3 +1,9 @@
-export default function(state, action) {
-  
+// state argument is not an application state . only the  state
+// this reducer is reponsible for
+export default function(state=null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+    return action.payload;
+  }
+ return state
 }
